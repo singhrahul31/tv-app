49033c8951b7b3016f4863f3046441c595dc2b96
Swatilekha <rahulsingh@Swatilekha.local>
2021-05-17 18:51:54 +0100

    initial commit

---
+++ src/main/java/com/tvapp/TvAppApplication.java
@@ -0,0 +1,13 @@
+package com.tvapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tvapp/TvAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tvapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
